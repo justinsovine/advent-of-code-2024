22f2f311393232c744235dc2a02157acb1ecde1a
Justin Sovine <justinsovine@gmail.com>
2024-12-02 00:16:16 -0500

    Day 02 init!

--- day02/solution.php
+++ day02/solution.php
@@ -3,6 +3,8 @@
 /**
  * Day 2 - Part 1
  *
- * ...
- *
+ * Determine the number of safe reports based on these rules:
+ * 1. The levels in a report must be entirely increasing or entirely decreasing.
+ * 2. The difference between any two adjacent levels must be at least 1 and at most 3.
  */
+
