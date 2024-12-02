5099f1bbf9e367625eaf23f405f7ae36ef386c67
Justin Sovine <justinsovine@gmail.com>
2024-12-02 11:24:02 -0500

    Day 2 - Part 1 initial solution complete

--- day02/solution.php
+++ day02/solution.php
@@ -8,3 +8,72 @@
  * 2. The difference between any two adjacent levels must be at least 1 and at most 3.
  */
 
+function read_input_file($input_file) : array
+{
+    // Open file
+    $fp = @fopen($input_file, "r");
+    if (!$fp) {
+        die("Error: Unable to open file " . $input_file. "\n");
+    }
+
+    // Extract reports into multidimensional array
+    $reports = [];
+    while (($buffer = fgets($fp)) !== false) {
+        $reports[] = array_map('intval', explode(" ", $buffer));
+    }
+
+    // Check if EOF and close file
+    if (!feof($fp)) {
+        die("Error: unexpected fgets() fail\n");
+    }
+    fclose($fp);
+
+    return $reports;
+}
+
+function report_checker($report) : bool
+{
+    $trend = '';
+    for ($i = 0; $i < count($report) - 1; $i++) {
+        // Check if increased or decreased in value
+        //echo $report[$i] . " - " . $report[$i + 1] . "\n";
+        if ($report[$i] > $report[$i + 1]) {
+            // Check if trend changed
+            if ($trend == 'decrease') {
+                return false; // Not safe!
+            }
+            $trend = 'increase';
+        } elseif ($report[$i] == $report[$i + 1]) {
+            // Did not increase at all
+            return false; // Not safe!
+        } else {
+            // Check if trend changed
+            if ($trend == 'increase') {
+                return false; // Not safe!
+            }
+            $trend = 'decrease';
+        }
+
+        // Check if value change was greater than 3
+        if (abs($report[$i] - $report[$i + 1]) > 3) {
+            return false; // Not safe!
+        }
+    }
+
+    return true; // Safe!
+}
+
+// Reads and parses the input file into a multidimensional array of reports
+$reports = read_input_file(__DIR__ . "/input.txt");
+
+$total_safe_reports = 0;
+foreach ($reports as $report) {
+    if (report_checker($report)) {
+        //echo "Safe!\n";
+        $total_safe_reports += 1;
+    } else {
+        //echo "Not safe!\n";
+    }
+}
+
+echo "Total Safe Reports: $total_safe_reports\n";
